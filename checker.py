7ad6959380ce580e3f5e874015b3a047b3e480e7
Love Hirdman <lohi@njord.nifelheim.ws>
2017-04-11 16:46:48 +0200

    Got it working

--- checker.py
+++ checker.py
@@ -8,7 +8,7 @@ import OpenSSL
 import socket
 from netaddr import *
 
-def get_enddate( hostip ):
+def get_enddate( ip ):
     color = {
             False: "\033[31;1m",
             True: "\033[32;1m",
@@ -17,34 +17,31 @@ def get_enddate( hostip ):
             }
     ctx = OpenSSL.SSL.Context(ssl.PROTOCOL_TLSv1)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
     x509 = None
 
     try:
-        s.connect((hostip, 443))
+        s.connect((ip,443))
         cnx = OpenSSL.SSL.Connection(ctx, s)
-        cnx.set_tlsext_host_name(hostip)
+        cnx.set_tlsext_host_name(ip)
         cnx.set_connect_state()
         cnx.do_handshake()
-
         x509 = cnx.get_peer_certificate()
         s.close()
     except Exception as e:
-        print "%30s: %s%s%s" % (hostip, color['error'], e, color['end'])
+        print "%30s: %s%s%s" % (ip, color['error'], e, color['end'])
 
     issuer = x509.get_issuer()
     issuer_corp = x509.get_issuer().organizationName
     issuer_url = x509.get_issuer().organizationalUnitName
     issuer_x509 = x509.get_issuer().commonName
     server_name = x509.get_subject().commonName
-
+#    print "issuer:" + issuer
     now = datetime.now(pytz.utc)
     begin = datetime.strptime(x509.get_notBefore(), "%Y%m%d%H%M%SZ").replace(tzinfo=pytz.UTC)
     begin_ok = begin < now
     end = datetime.strptime(x509.get_notAfter(), "%Y%m%d%H%M%SZ").replace(tzinfo=pytz.UTC)
     end_ok = end > now
-
-    print "%30s: begin=%s%s%s end=%s%s%s issuer=%s" % (hostip,
+    print "%30s: certificate=%s begin=%s%s%s end=%s%s%s issuer=%s" % (ip, server_name, 
             color[begin_ok], begin.strftime("%d.%m.%Y"), color['end'],
             color[end_ok], end.strftime("%d.%m.%Y"), color['end'],
             issuer_corp)
@@ -53,16 +50,16 @@ def test_conn( ip ):
     try:
         socket.gethostbyaddr(ip)
     except socket.herror:
-        print "Connection failed:" + ip
+#        print "Connection failed:" + ip
         return False
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(2)
         sock.connect((ip,443))
-    except socket.timeout:
-        print "Host up, port 443 not available for:" + ip
+    except socket.error:
+#        print "Host up, port 443 not available for:" + ip
         return False
-    print "Connection successful:" + ip
+#    print "Connection successful:" + ip
     return True
 
 
